a5e136d964311cacc54c75f86859f832ca1d78e1
gameDevVab <vaibhavsharmadev31@gmail.com>
2025-12-14 11:55:25 -0600

    Update Player Script
    Added Debug

--- Example/Assets/Scripts/Player.cs
+++ Example/Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("hello World");
     }
 
     // Update is called once per frame
